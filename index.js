b8d32a37c6ca294a250f747ee5d6710b4fec8280
SiveleleN <sivelelenkamane29@gmail.com>
2023-11-27 14:56:33 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+ 
+ let numbers = [5, 8, 2, 10, 4, 3, 7, 9, 1];
+r
+ function findMin() {
+     let min = Math.min(...numbers);
+     alert("The minimum number is: " + min);
+ }
+
+ 
+ function findMax() {
+     let max = Math.max(...numbers);
+     alert("The maximum number is: " + max);
+ }
